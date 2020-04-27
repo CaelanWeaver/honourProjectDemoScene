0f2eaea65666fb520b3b300aaf28f58ccd45b42e
keendawg <caelanweaver@live.co.uk>
2020-04-27 01:23:48 +0100

    project up-to-date

---
+++ Assets/Samples/Farm/Scripts/SetAnswer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SetAnswer : MonoBehaviour {
+
+    [SerializeField]
+    private MeshRenderer boardRend;
+    [SerializeField]
+    private Material[] materials;
+
+    public UnityEvent blockAEntered;
+    public UnityEvent blockBEntered;
+
+    private void Start()
+    {
+        if(blockAEntered == null)
+        {
+            blockAEntered = new UnityEvent();
+        }
+
+        if (blockBEntered == null)
+        {
+            blockBEntered = new UnityEvent();
+        }
+
+        blockAEntered.AddListener(BlockASelected); // answer selected is a subscriber to the event
+
+        blockBEntered.AddListener(BlockBSelected); 
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "BlockA")
+        {
+            blockAEntered.Invoke();
+
+        }
+        else if(col.gameObject.tag == "BlockB")
+        {
+            blockBEntered.Invoke();
+
+        }
+    }
+
+    private void BlockASelected()
+    {
+
+        Debug.Log("Block A Entered");
+
+    }
+
+
+    private void BlockBSelected()
+    {
+
+        Debug.Log("Block B Entered");
+
+    }
+}
