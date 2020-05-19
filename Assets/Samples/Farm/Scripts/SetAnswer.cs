7d69e33f57ff30612778dcf53f06798088dd5bf6
keendawg <caelanweaver@live.co.uk>
2020-05-19 15:55:10 +0100

    final changes to deliverable

--- Assets/Samples/Farm/Scripts/SetAnswer.cs
+++ Assets/Samples/Farm/Scripts/SetAnswer.cs
@@ -13,49 +13,50 @@ public class SetAnswer : MonoBehaviour {
     public UnityEvent blockAEntered;
     public UnityEvent blockBEntered;
 
+                                        //Below, you can see the code used to create the unity events system
     private void Start()
-    {
-        if(blockAEntered == null)
+    {                                                         
+        if(blockAEntered == null) //checks if blockAEntered does not exist. if not, we instatiate a singleton of blockAEntered
         {
-            blockAEntered = new UnityEvent();
+            blockAEntered = new UnityEvent(); //Declares blockAEntered as a new UnityEvent()
         }
 
-        if (blockBEntered == null)
+        if (blockBEntered == null) //checks if blockBEntered does not exist. if not, we instatiate a singleton of blockBEntered
         {
-            blockBEntered = new UnityEvent();
+            blockBEntered = new UnityEvent(); //Declares blockBEntered as a new UnityEvent()
         }
 
-        blockAEntered.AddListener(BlockASelected); // answer selected is a subscriber to the event
+        blockAEntered.AddListener(BlockASelected); // BlockASelected is a subscriber to the event blockAEntered.AddListener()
 
-        blockBEntered.AddListener(BlockBSelected); 
+        blockBEntered.AddListener(BlockBSelected); // BlockBSelected is a subscriber to the event blockBEntered.AddListener()
     }
 
-    private void OnTriggerEnter(Collider col)
+    private void OnTriggerEnter(Collider col) //This is the collider detection function
     {
-        if (col.gameObject.tag == "BlockA")
+        if (col.gameObject.tag == "BlockA") //If we collide with the game object tagged 'BlockA'
         {
-            blockAEntered.Invoke();
+            blockAEntered.Invoke(); //We invoke all registered callbacks of the blockAEntered Unity Event
 
         }
-        else if(col.gameObject.tag == "BlockB")
+        else if(col.gameObject.tag == "BlockB") //If we collide with the game object tagged 'BlockB'
         {
-            blockBEntered.Invoke();
+            blockBEntered.Invoke(); //We invoke all registered callbacks of the blockBEntered Unity Event
 
         }
     }
 
-    private void BlockASelected()
+    private void BlockASelected() //Method passed as the subscriber in the blockAEntered event
     {
 
-        Debug.Log("Block A Entered");
+        Debug.Log("Block A Entered"); //Console log used to inform developer that block A has entered whiteboard trigger space
 
     }
 
 
-    private void BlockBSelected()
+    private void BlockBSelected() //Method passed as the subscriber in the blockBEntered event
     {
 
-        Debug.Log("Block B Entered");
+        Debug.Log("Block B Entered"); //Console log used to inform developer that block B has entered whiteboard trigger space
 
     }
 }
